3a3fa401e766710d67a7d752a5abedd975987c69
Minjae Kim <alswo5052@gmail.com>
2020-05-17 22:23:26 +0900

    메인 더보기 화면 version 정보 디스플레이 추가

--- app/src/main/java/team/triplog/MainMoreFragment.java
+++ app/src/main/java/team/triplog/MainMoreFragment.java
@@ -4,13 +4,29 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 public class MainMoreFragment extends Fragment {
+    private View rootView;
+    private TextView textVersion;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main_more, container, false);
+        rootView = inflater.inflate(R.layout.fragment_main_more, container, false);
+
+        init();
+        setUi();
+
+        return rootView;
+    }
+
+    private void init() {
+        textVersion = rootView.findViewById(R.id.text_version);
+    }
+
+    private void setUi() {
+        textVersion.setText(getString(R.string.main_more_version, BuildConfig.VERSION_NAME));
     }
 }
