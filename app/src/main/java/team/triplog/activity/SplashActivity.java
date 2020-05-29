9d9b911ae06dd48ed4665ced63465d5cec72143d
Minjae Kim <alswo5052@gmail.com>
2020-05-30 02:04:46 +0900

    Splash 화면 로고 애니메이션 및 화면전환 적용

--- app/src/main/java/team/triplog/activity/SplashActivity.java
+++ app/src/main/java/team/triplog/activity/SplashActivity.java
@@ -4,12 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import team.triplog.R;
 
 public class SplashActivity extends AppCompatActivity {
+    long DURATION = 1500L;
     View viewLogo;
 
     @Override
@@ -27,5 +30,32 @@ public class SplashActivity extends AppCompatActivity {
 
     private void init() {
         viewLogo = findViewById(R.id.view_logo);
+        setAnimation();
+    }
+
+    private void setAnimation() {
+        AlphaAnimation anim = new AlphaAnimation(0.0f, 1.0f);
+        anim.setDuration(DURATION);
+        anim.setStartOffset(0);
+        anim.setRepeatMode(Animation.REVERSE);
+        anim.setRepeatCount(Animation.ABSOLUTE);
+        anim.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        viewLogo.startAnimation(anim);
     }
 }
