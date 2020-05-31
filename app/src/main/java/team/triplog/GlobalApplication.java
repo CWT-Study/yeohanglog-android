77b1da3fb55405a3d811e2044a54bc5a9282353e
Minjae Kim <alswo5052@gmail.com>
2020-05-31 11:56:23 +0900

    Application 클래스 추가

---
+++ app/src/main/java/team/triplog/GlobalApplication.java
@@ -0,0 +1,13 @@
+package team.triplog;
+
+import android.app.Application;
+
+import com.kakao.sdk.common.KakaoSdk;
+
+public class GlobalApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
