23cf021284770c5160ebf302184f38ec9252d65c
Minjae Kim <alswo5052@gmail.com>
2020-06-01 16:37:33 +0900

    카카오 로그인 및 로그인 성공 후 유저정보 받아오는 로직 추가

--- app/src/main/java/team/triplog/activity/SignInActivity.java
+++ app/src/main/java/team/triplog/activity/SignInActivity.java
@@ -2,6 +2,7 @@ package team.triplog.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -9,15 +10,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.kakao.auth.AuthType;
+import com.kakao.auth.ISessionCallback;
 import com.kakao.auth.Session;
+import com.kakao.network.ErrorResult;
+import com.kakao.usermgmt.UserManagement;
+import com.kakao.usermgmt.callback.MeV2ResponseCallback;
+import com.kakao.usermgmt.response.MeV2Response;
+import com.kakao.usermgmt.response.model.Profile;
+import com.kakao.usermgmt.response.model.UserAccount;
+import com.kakao.util.OptionalBoolean;
+import com.kakao.util.exception.KakaoException;
 
-import team.triplog.KakaoSessionCallback;
 import team.triplog.R;
 
 public class SignInActivity extends AppCompatActivity {
     private Session session;
     private ConstraintLayout buttonKakao;
-    private KakaoSessionCallback sessionCallback = new KakaoSessionCallback();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,13 +42,86 @@ public class SignInActivity extends AppCompatActivity {
     }
 
     private void init() {
-        buttonKakao = findViewById(R.id.button_sign_in_kakao_talk);
-
         session = Session.getCurrentSession();
         session.addCallback(sessionCallback);
 
+        buttonKakao = findViewById(R.id.button_sign_in_kakao_talk);
         buttonKakao.setOnClickListener(onClickListener);
     }
+
+    private void getUser() {
+        UserManagement.getInstance()
+                .me(new MeV2ResponseCallback() {
+                    @Override
+                    public void onSessionClosed(ErrorResult errorResult) {
+                        Log.e("KAKAO_API", "세션이 닫혀 있음: " + errorResult);
+                    }
+
+                    @Override
+                    public void onFailure(ErrorResult errorResult) {
+                        Log.e("KAKAO_API", "사용자 정보 요청 실패: " + errorResult);
+                    }
+
+                    @Override
+                    public void onSuccess(MeV2Response result) {
+                        Log.i("KAKAO_API", "사용자 아이디: " + result.getId());
+
+                        UserAccount kakaoAccount = result.getKakaoAccount();
+                        if (kakaoAccount != null) {
+
+                            // 이메일
+                            String email = kakaoAccount.getEmail();
+
+                            if (email != null) {
+                                Log.i("KAKAO_API", "email: " + email);
+
+                            } else if (kakaoAccount.emailNeedsAgreement() == OptionalBoolean.TRUE) {
+                                // 동의 요청 후 이메일 획득 가능
+                                // 단, 선택 동의로 설정되어 있다면 서비스 이용 시나리오 상에서 반드시 필요한 경우에만 요청해야 합니다.
+
+                            } else {
+                                // 이메일 획득 불가
+                            }
+
+                            // 프로필
+                            Profile profile = kakaoAccount.getProfile();
+
+                            if (profile != null) {
+                                Log.d("KAKAO_API", "nickname: " + profile.getNickname());
+                                Log.d("KAKAO_API", "profile image: " + profile.getProfileImageUrl());
+                                Log.d("KAKAO_API", "thumbnail image: " + profile.getThumbnailImageUrl());
+
+                                callActivity();
+
+                            } else if (kakaoAccount.profileNeedsAgreement() == OptionalBoolean.TRUE) {
+                                // 동의 요청 후 프로필 정보 획득 가능
+
+                            } else {
+                                // 프로필 획득 불가
+                            }
+                        }
+                    }
+                });
+    }
+
+    private void callActivity() {
+        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private ISessionCallback sessionCallback = new ISessionCallback() {
+        @Override
+        public void onSessionOpened() {
+            Log.i("KAKAO_SESSION", "onSessionOpened : ");
+            getUser();
+        }
+
+        @Override
+        public void onSessionOpenFailed(KakaoException exception) {
+            Log.e("KAKAO_SESSION", "onSessionOpenFailed : ", exception);
+        }
+    };
 
     private View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
