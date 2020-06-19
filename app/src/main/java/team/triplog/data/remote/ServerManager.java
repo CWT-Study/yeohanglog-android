006e99d0a66dc9bad54ee1781ccabad5d4ccae0b
Minjae Kim <alswo5052@gmail.com>
2020-06-19 18:35:24 +0900

    ServerManager 에서 retrofit 설정부분 분리
    - ServerDefine 구조 설정

--- app/src/main/java/team/triplog/data/remote/ServerManager.java
+++ app/src/main/java/team/triplog/data/remote/ServerManager.java
@@ -1,22 +1,11 @@
 package team.triplog.data.remote;
 
-import okhttp3.OkHttpClient;
-import okhttp3.logging.HttpLoggingInterceptor;
 import retrofit2.Response;
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ServerManager {
-    private static String BASE_URL = "https://google.com";
-
-    private static ServerInterface server = new Retrofit.Builder()
-            .client(new OkHttpClient.Builder().addInterceptor(new HttpLoggingInterceptor()).build())
-            .addConverterFactory(GsonConverterFactory.create())
-            .baseUrl(BASE_URL)
-            .build()
-            .create(ServerInterface.class);
+    private static ServerInterface SERVER = ServerDefine.retrofit.create(ServerInterface.class);
 
     Response<UserResponse> getUser() {
-        return server.getUser();
+        return SERVER.getUser();
     }
 }
