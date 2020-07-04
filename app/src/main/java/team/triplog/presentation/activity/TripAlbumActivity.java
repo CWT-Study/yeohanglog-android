58a2b0a661a84df2a6c1825e3b65e80ebaf557b0
Minjae Kim <alswo5052@gmail.com>
2020-07-04 15:10:33 +0900

    여행 일정 사진첩 화면 구성 및 연동

---
+++ app/src/main/java/team/triplog/presentation/activity/TripAlbumActivity.java
@@ -0,0 +1,51 @@
+package team.triplog.presentation.activity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import team.triplog.R;
+import team.triplog.presentation.util.TextCountAnimation;
+
+public class TripAlbumActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trip_album);
+
+        init();
+        setData();
+        setUi();
+    }
+
+    private void init() {
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+    }
+
+    private void setData() {
+    }
+
+    private void setUi() {
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
