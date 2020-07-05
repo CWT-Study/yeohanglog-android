76f9b81c591941c6eb584299ebaac57426e3adee
Minjae Kim <alswo5052@gmail.com>
2020-07-05 23:58:13 +0900

    프로필 설정 화면 유저 데이터 디스플레이 적용

--- app/src/main/java/team/triplog/presentation/activity/MoreUserActivity.java
+++ app/src/main/java/team/triplog/presentation/activity/MoreUserActivity.java
@@ -2,13 +2,25 @@ package team.triplog.presentation.activity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.widget.Toolbar;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+
 import team.triplog.R;
+import team.triplog.data.local.User;
 
 public class MoreUserActivity extends BaseActivity {
     private Toolbar toolbar;
+    private TextView textUserName;
+    private TextView textUserCode;
+    private TextView textUserAccount;
+    private ImageView viewUserProfile;
+    private User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +34,10 @@ public class MoreUserActivity extends BaseActivity {
 
     private void init() {
         toolbar = findViewById(R.id.toolbar);
+        textUserName = findViewById(R.id.text_user_name);
+        textUserCode = findViewById(R.id.text_user_code);
+        textUserAccount = findViewById(R.id.text_user_account);
+        viewUserProfile = findViewById(R.id.view_user_profile);
 
         setSupportActionBar(toolbar);
         if (getSupportActionBar() != null) {
@@ -32,9 +48,17 @@ public class MoreUserActivity extends BaseActivity {
     }
 
     private void setData() {
+        user = realm.where(User.class).findFirst();
     }
 
     private void setUi() {
+        textUserName.setText(user.getName());
+
+        Glide.with(this)
+                .load(user.getImage())
+                .apply(RequestOptions.bitmapTransform(new RoundedCorners(16)))
+                .thumbnail(0.1f)
+                .into(viewUserProfile);
     }
 
     @Override
