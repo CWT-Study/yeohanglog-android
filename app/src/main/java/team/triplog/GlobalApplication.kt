95e063fb34fabd26db2e1310cd87dc6945c9ccb4
Minjae Kim <alswo5052@gmail.com>
2020-09-25 22:27:58 +0900

    Koin 설정

---
+++ app/src/main/java/team/triplog/GlobalApplication.kt
@@ -0,0 +1,74 @@
+package team.triplog
+
+import android.app.Application
+import com.kakao.auth.*
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
+import team.triplog.data.local.base.RealmManager
+import team.triplog.di.mainModule
+
+class GlobalApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        instance = this
+        KakaoSDK.init(KakaoSDKAdapter())
+        RealmManager.setRealmConfig(this)
+
+        setupKoin()
+    }
+
+    override fun onTerminate() {
+        super.onTerminate()
+        instance = null
+    }
+
+    class KakaoSDKAdapter : KakaoAdapter() {
+        override fun getSessionConfig(): ISessionConfig {
+            return object : ISessionConfig {
+                override fun getAuthTypes(): Array<AuthType> {
+                    return arrayOf(AuthType.KAKAO_LOGIN_ALL)
+                }
+
+                override fun isUsingWebviewTimer(): Boolean {
+                    return false
+                }
+
+                override fun isSecureMode(): Boolean {
+                    return false
+                }
+
+                override fun getApprovalType(): ApprovalType? {
+                    return ApprovalType.INDIVIDUAL
+                }
+
+                override fun isSaveFormData(): Boolean {
+                    return true
+                }
+            }
+        }
+
+        override fun getApplicationConfig(): IApplicationConfig {
+            return IApplicationConfig { getInstance()!! }
+        }
+    }
+
+    private fun setupKoin() {
+        startKoin {
+            androidContext(this@GlobalApplication)
+            modules(
+                listOf(
+                    mainModule
+                )
+            )
+        }
+    }
+
+    companion object {
+        private var instance: GlobalApplication? = null
+        fun getInstance(): GlobalApplication? {
+            checkNotNull(instance) { "This Application does not inherit com.kakao.GlobalApplication" }
+            return instance
+        }
+    }
+}
---
+++ app/src/main/java/team/triplog/di/MainModule.kt
@@ -0,0 +1,6 @@
+package team.triplog.di
+
+import org.koin.dsl.module
+
+val mainModule = module {
+}
