0ebfcf1d927bca91827b55b71a0f57b5730cf490
Minjae Kim <alswo5052@gmail.com>
2020-11-04 23:04:37 +0900

    Splash 변경
    - 스플래시의 목적처럼 activity 형태가 아닌 theme 형태로 변경
    - 아이콘 추가

---
+++ app/src/main/java/team/triplog/presentation/splash/SplashActivity.kt
@@ -0,0 +1,22 @@
+package team.triplog.presentation.splash
+
+import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import team.triplog.presentation.activity.startMainActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Handler().postDelayed({ moveNext() }, 300)
+    }
+
+    private fun moveNext() {
+        // TODO : SharedPreference 로 로그인 관리 필요
+        // TODO : Login 된 UserId 가 있을 경우, 분기처리 필요
+        startMainActivity()
+        finish()
+    }
+}
