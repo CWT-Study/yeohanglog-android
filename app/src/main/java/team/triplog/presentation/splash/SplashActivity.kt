41db9687046dbce537bc6dac02c27a79cc86409d
Minjae Kim <alswo5052@gmail.com>
2020-11-04 23:43:14 +0900

    Splash 에서 Main 으로 이동할 때, animation 추가
    - fade in, fade out 적용
    - 애니메이션 시간 조정

--- app/src/main/java/team/triplog/presentation/splash/SplashActivity.kt
+++ app/src/main/java/team/triplog/presentation/splash/SplashActivity.kt
@@ -3,6 +3,7 @@ package team.triplog.presentation.splash
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
+import team.triplog.R
 import team.triplog.presentation.activity.startMainActivity
 
 class SplashActivity : AppCompatActivity() {
@@ -10,13 +11,14 @@ class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        Handler().postDelayed({ moveNext() }, 300)
+        Handler().postDelayed({ moveNext() }, 500)
     }
 
     private fun moveNext() {
         // TODO : SharedPreference 로 로그인 관리 필요
         // TODO : Login 된 UserId 가 있을 경우, 분기처리 필요
         startMainActivity()
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out)
         finish()
     }
 }
