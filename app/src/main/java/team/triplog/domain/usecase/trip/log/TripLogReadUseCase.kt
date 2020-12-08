d8343c0eccfcd2ff009e0b7a81683430601a23f0
Minjae Kim <alswo5052@gmail.com>
2020-12-09 00:32:10 +0900

    메인화면 하단 여행 톺아보기 RecyclerView UI 적용
    - TripLog 데이터를 Trip 으로 통합하여 관리하도록 설정
    - 추후 데이터 연동시 테스트코드 제거 필요

--- app/src/main/java/team/triplog/domain/usecase/trip/log/TripLogReadUseCase.kt
+++ app/src/main/java/team/triplog/domain/usecase/trip/log/TripLogReadUseCase.kt
@@ -1,7 +1,7 @@
 package team.triplog.domain.usecase.trip.log
 
 import team.triplog.data.repository.TripLogRepository
-import team.triplog.data.source.local.entity.TripLog
+import team.triplog.data.source.local.entity.Trip
 
 
 /**
@@ -10,17 +10,17 @@ import team.triplog.data.source.local.entity.TripLog
 class TripLogReadUseCase(private val tripLogRepository: TripLogRepository) {
 
     /** 여행 기록 전체 가져오기 */
-    fun getAllTripLogs(): List<TripLog> {
+    fun getAllTripLogs(): List<Trip> {
         return tripLogRepository.readAllTripLogs()
     }
 
     /** 특정 개수만큼 여행 기록 가져오기 */
-    fun getTripLogs(count: Int): List<TripLog> {
+    fun getTripLogs(count: Int): List<Trip> {
         return tripLogRepository.readTripLogs(count)
     }
 
     /** 여행 기록 가져오기 */
-    fun getTripLog(id: Long): TripLog? {
+    fun getTripLog(id: Long): Trip? {
         return tripLogRepository.readTripLog(id)
     }
 
