1e52af4bd0f157af1abab5414ca80aba706531a8
Minjae Kim <alswo5052@gmail.com>
2021-01-10 23:38:54 +0900

    기본으로 사용될 AlertDialog 구성

--- app/src/main/java/team/triplog/presentation/base/BaseFragment.kt
+++ app/src/main/java/team/triplog/presentation/base/BaseFragment.kt
@@ -1,5 +1,26 @@
 package team.triplog.presentation.base
 
+import android.app.AlertDialog
+import android.content.DialogInterface
 import androidx.fragment.app.Fragment
 
-open class BaseFragment : Fragment()
+open class BaseFragment : Fragment() {
+
+    fun showAlert(
+        message: String,
+        title: String? = null,
+        positive: String? = null,
+        negative: String? = null,
+        positiveAction: ((DialogInterface, Int) -> Unit)? = null,
+        negativeAction: ((DialogInterface, Int) -> Unit)? = null
+    ) {
+        context?.let {
+            AlertDialog.Builder(it).apply {
+                setMessage(message)
+                title?.let { title -> setTitle(title) }
+                positive?.let { positive -> setPositiveButton(positive, positiveAction) }
+                negative?.let { negative -> setNegativeButton(negative, negativeAction) }
+            }.show()
+        }
+    }
+}
