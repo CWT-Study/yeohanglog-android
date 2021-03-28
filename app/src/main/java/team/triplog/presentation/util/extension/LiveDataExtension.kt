a9e551ca8f1b6192334d0795a9a556a1e47af5e0
Minjae Kim <alswo5052@gmail.com>
2021-03-29 01:51:54 +0900

    LiveData debounce 확장함수 추가

---
+++ app/src/main/java/team/triplog/presentation/util/extension/LiveDataExtension.kt
@@ -0,0 +1,26 @@
+package team.triplog.presentation.util.extension
+
+import android.os.Handler
+import android.os.Looper
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+
+
+/**
+ *  @author Man-jae
+ *  @since 2021.03.13.
+ */
+
+fun <T> LiveData<T>.debounce(duration: Long = 1000L) = MediatorLiveData<T>().also { mld ->
+    val source = this
+    val handler = Handler(Looper.getMainLooper())
+
+    val runnable = Runnable {
+        mld.value = source.value
+    }
+
+    mld.addSource(source) {
+        handler.removeCallbacks(runnable)
+        handler.postDelayed(runnable, duration)
+    }
+}
