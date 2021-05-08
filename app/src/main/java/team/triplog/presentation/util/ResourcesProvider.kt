71301db2fa00a74594348d1d7768162f0e9d4ff9
Minjae Kim <alswo5052@gmail.com>
2021-05-08 21:26:41 +0900

    feat: Resource 관리를 위한 provider 추가
    fixed #83

---
+++ app/src/main/java/team/triplog/presentation/util/ResourcesProvider.kt
@@ -0,0 +1,38 @@
+package team.triplog.presentation.util
+
+import android.content.Context
+import androidx.annotation.ColorInt
+import androidx.annotation.ColorRes
+import androidx.annotation.DimenRes
+import androidx.annotation.StringRes
+import androidx.core.content.ContextCompat
+
+interface ResourcesProvider {
+    @ColorInt
+    fun getColor(@ColorRes resId: Int): Int
+    fun getString(@StringRes resId: Int): String
+    fun getString(@StringRes resId: Int, vararg args: Any): String
+    fun getDimen(@DimenRes resId: Int): Float
+    fun getDimenInt(@DimenRes resId: Int): Int
+
+    //... other things, above methods just for the example
+}
+
+@Suppress("TooManyFunctions")
+inline class AppResourcesProvider(
+    private val context: Context
+) : ResourcesProvider {
+    @ColorInt
+    override fun getColor(resId: Int) = ContextCompat.getColor(context, resId)
+
+    override fun getString(resId: Int) = context.getString(resId)
+
+    override fun getString(resId: Int, vararg args: Any) = context.getString(resId, *args)
+
+    override fun getDimen(resId: Int): Float = context.resources.getDimension(resId)
+
+    override fun getDimenInt(resId: Int) = context.resources.getDimensionPixelSize(resId)
+
+    //implementation for another methods
+
+}
