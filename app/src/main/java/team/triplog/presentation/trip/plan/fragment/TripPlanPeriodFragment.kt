d86caffb8c5cc1e11cfed0f40539a94d22a5ff44
Minjae Kim <alswo5052@gmail.com>
2021-05-19 19:00:38 +0900

    chore: 여행 기간 설정 화면 viewModel 필드 추가

--- app/src/main/java/team/triplog/presentation/trip/plan/fragment/TripPlanPeriodFragment.kt
+++ app/src/main/java/team/triplog/presentation/trip/plan/fragment/TripPlanPeriodFragment.kt
@@ -1,8 +1,10 @@
 package team.triplog.presentation.trip.plan.fragment
 
+import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import team.triplog.R
 import team.triplog.databinding.FragmentTripPlanPeriodBinding
 import team.triplog.presentation.base.BaseFragment
+import team.triplog.presentation.viewmodel.TripPlanInfoViewModel
 
 
 /**
@@ -12,6 +14,8 @@ import team.triplog.presentation.base.BaseFragment
 class TripPlanPeriodFragment : BaseFragment<FragmentTripPlanPeriodBinding>(
     R.layout.fragment_trip_plan_period
 ) {
+    private val tripPlanInfoViewModel: TripPlanInfoViewModel by sharedViewModel()
+
     override fun setup() {
     }
 }
