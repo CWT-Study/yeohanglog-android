c35a8ac28e2b1fa1ca27e051978c2a5e75c484b3
Minjae Kim <alswo5052@gmail.com>
2021-05-19 14:40:44 +0900

    chore: UI 변경사항 적용
    - BaseViewModel 을 통해 키보드 off 조정하도록 설정

--- app/src/main/java/team/triplog/presentation/base/BaseViewModel.kt
+++ app/src/main/java/team/triplog/presentation/base/BaseViewModel.kt
@@ -3,6 +3,7 @@ package team.triplog.presentation.base
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import team.triplog.presentation.util.event.Event
 
 
 /**
@@ -11,11 +12,20 @@ import androidx.lifecycle.ViewModel
  */
 abstract class BaseViewModel : ViewModel() {
 
+    private val _eventClickBackground = MutableLiveData<Event<Unit>>()
+    val eventClickBackground: LiveData<Event<Unit>>
+        get() = _eventClickBackground
+
     private val _isLoading = MutableLiveData<Boolean>(false)
     val isLoading: LiveData<Boolean>
         get() = _isLoading
 
+
     protected fun setLoading(set: Boolean) {
         _isLoading.value = set
+    }
+
+    fun clickBackground() {
+        _eventClickBackground.value = Event(Unit)
     }
 }
--- app/src/main/java/team/triplog/presentation/trip/plan/fragment/TripPlanNameFragment.kt
+++ app/src/main/java/team/triplog/presentation/trip/plan/fragment/TripPlanNameFragment.kt
@@ -1,14 +1,12 @@
 package team.triplog.presentation.trip.plan.fragment
 
-import androidx.core.view.isVisible
-import androidx.lifecycle.Observer
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import team.triplog.R
 import team.triplog.databinding.FragmentTripPlanNameBinding
 import team.triplog.presentation.base.BaseFragment
+import team.triplog.presentation.util.event.EventObserver
 import team.triplog.presentation.util.extension.hideKeyboard
 import team.triplog.presentation.util.extension.setupButton
-import team.triplog.presentation.util.extension.showKeyboard
 import team.triplog.presentation.viewmodel.TripPlanInfoViewModel
 
 
@@ -29,21 +27,14 @@ class TripPlanNameFragment : BaseFragment<FragmentTripPlanNameBinding>(
     private fun setViewModel() {
         binding.viewModel = tripPlanInfoViewModel
 
-        tripPlanInfoViewModel.hasName.observe(viewLifecycleOwner, Observer {
-            binding.groupTripName.isVisible = !it
-            binding.groupTripPeriod.isVisible = it
-            when (it) {
-                true -> binding.tietTripName.hideKeyboard()
-                else -> binding.tietTripName.showKeyboard()
+        tripPlanInfoViewModel.eventClickBackground.observe(
+            viewLifecycleOwner, EventObserver {
+                binding.tietTripName.hideKeyboard()
             }
-        })
+        )
     }
 
     private fun setView() {
-        binding.clMain.setOnClickListener {
-            binding.tietTripName.hideKeyboard()
-        }
-
         binding.clNext.setupButton()
         binding.clNext.setOnClickListener {
             tripPlanInfoViewModel.setName(true)
