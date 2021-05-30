ad78b855197302a90e109dc99131bb1f7b0b1e85
Minjae Kim <alswo5052@gmail.com>
2021-05-30 21:25:52 +0900

    메인 프로필 화면 프로필 viewModel 연동

--- app/src/main/java/team/triplog/presentation/main/fragment/MainProfileFragment.kt
+++ app/src/main/java/team/triplog/presentation/main/fragment/MainProfileFragment.kt
@@ -4,13 +4,16 @@ import org.koin.androidx.viewmodel.ext.android.sharedViewModel
 import team.triplog.R
 import team.triplog.databinding.FragmentMainProfileBinding
 import team.triplog.presentation.base.BaseFragment
+import team.triplog.presentation.util.event.EventObserver
 import team.triplog.presentation.util.extension.setupButton
+import team.triplog.presentation.viewmodel.ProfileViewModel
 import team.triplog.presentation.viewmodel.UserViewModel
 
 class MainProfileFragment : BaseFragment<FragmentMainProfileBinding>(
     R.layout.fragment_main_profile
 ) {
     private val userViewModel: UserViewModel by sharedViewModel()
+    private val profileViewModel: ProfileViewModel by sharedViewModel()
 
     override fun setup() {
         setupViewModel()
@@ -19,10 +22,21 @@ class MainProfileFragment : BaseFragment<FragmentMainProfileBinding>(
 
     private fun setupViewModel() {
         binding.userViewModel = userViewModel
+        binding.profileViewModel = profileViewModel
+
+        profileViewModel.clickMoveSignIn.observe(
+            viewLifecycleOwner, EventObserver {
+                moveSignIn()
+            }
+        )
     }
 
     private fun setupView() {
         binding.clBtnSignIn.setupButton()
+    }
+
+    private fun moveSignIn() {
+        // TODO : 로그인 페이지로 이동 
     }
 
     override fun onResume() {
