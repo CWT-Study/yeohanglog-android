d943a68ec73e2112efa1585b5dc17230a7d2c83a
Minjae Kim <alswo5052@gmail.com>
2021-06-14 22:58:41 +0900

    chore: 불필요한 코드 제거

--- app/src/main/java/team/triplog/presentation/base/BaseActivity.kt
+++ app/src/main/java/team/triplog/presentation/base/BaseActivity.kt
@@ -1,24 +1,15 @@
 package team.triplog.presentation.base
 
-import android.Manifest
-import android.content.Intent
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
 import android.widget.Toast
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.LayoutRes
-import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import org.koin.androidx.viewmodel.ext.android.viewModel
 import team.triplog.presentation.viewmodel.ToolbarViewModel
-import timber.log.Timber
 
 
 /**
@@ -65,85 +56,5 @@ abstract class BaseActivity<T : ViewDataBinding>(
 
     protected fun showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
-    }
-
-    fun showCamera() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            requestPermission(Manifest.permission.CAMERA, REQUEST_IMAGE_CAPTURE) {
-                Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { takePictureIntent ->
-                    takePictureIntent.resolveActivity(packageManager)?.also {
-                        startForCameraResult.launch(takePictureIntent)
-                    }
-                }
-            }
-        }
-    }
-
-    @RequiresApi(Build.VERSION_CODES.M)
-    fun requestPermission(
-        permission: String,
-        requestCode: Int,
-        granted: () -> Unit
-    ) {
-        when {
-            ContextCompat.checkSelfPermission(this, permission)
-                    == PackageManager.PERMISSION_GRANTED -> {
-                // You can use the API that requires the permission.
-                granted()
-            }
-            shouldShowRequestPermissionRationale(permission) -> {
-                // In an educational UI, explain to the user why your app requires this
-                // permission for a specific feature to behave as expected. In this UI,
-                // include a "cancel" or "no thanks" button that allows the user to
-                // continue using your app without granting the permission.
-            }
-            else -> {
-                // You can directly ask for the permission.
-                requestPermissions(arrayOf(permission), requestCode)
-            }
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        when (requestCode) {
-            REQUEST_IMAGE_CAPTURE -> {
-                // If request is cancelled, the result arrays are empty.
-                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-                    // Permission is granted. Continue the action or workflow
-                    // in your app.
-                } else {
-                    // Explain to the user that the feature is unavailable because
-                    // the features requires a permission that the user has denied.
-                    // At the same time, respect the user's decision. Don't link to
-                    // system settings in an effort to convince the user to change
-                    // their decision.
-                }
-                return
-            }
-
-            // Add other 'when' lines to check for other
-            // permissions this app might request.
-            else -> {
-                // Ignore all other requests.
-            }
-        }
-    }
-
-    private val startForCameraResult = registerForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) { result ->
-        when (result.resultCode) {
-            RESULT_OK -> Unit
-            RESULT_CANCELED -> Unit
-        }
-    }
-
-    companion object {
-        const val REQUEST_IMAGE_CAPTURE = 1
     }
 }
